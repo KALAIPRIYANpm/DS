148d00a0ec0d87e52e4f157a61e9ddcd5b240fda
KALAIPRIYAMpm <kalaipriyan7777@gmail.com>
2025-07-24 10:24:05 +0530

    insertion

--- list/DoublyLinkedList.java
+++ list/DoublyLinkedList.java
@@ -19,6 +19,21 @@ class Dll {
         nn.next = head;
         head = nn;
     }
+    void insertLast(int val){
+        if(head==null){
+            insertFirst(val);
+            return;
+        }
+        node nn = new node();
+        nn.data = val;
+        nn.next = null;
+        node temp = head;
+        while(temp.next!=null){
+            temp = temp.next;
+        }
+        temp.next = nn;
+        nn.prev = temp;
+    }
     
 
     void display() {
@@ -36,6 +51,7 @@ public class DoublyLinkedList {
         obj.insertFirst(5);
         obj.insertFirst(10);
         obj.insertFirst(15);
+        obj.insertLast(20);
         obj.display();
     }
 }
