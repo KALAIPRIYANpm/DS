5406a68cedafe1f2fb0c88b4cc228e9c8410acce
KALAIPRIYAMpm <kalaipriyan7777@gmail.com>
2025-07-26 15:37:21 +0530

    feat:search

--- list/binary.java
+++ list/binary.java
@@ -20,6 +20,21 @@ class BST {
         return root;
     }
 
+    boolean search(Node node,int val){
+        if(node==null){
+            return false;
+        }
+        if(node.data==val){
+            return true;
+        }
+        else if(val<root.data){
+            return search(root.left,val);
+        }
+        else{
+            return search(root.right,val);
+        }
+    }
+
     void inorder(Node root) {
         if (root != null) {
             inorder(root.left);
@@ -29,17 +44,16 @@ class BST {
     }
 }
 
-public class bst {
+public class binary{
     public static void main(String[] args) {
         BST obj = new BST();
-        
         obj.root = obj.insert(obj.root, 4);
         obj.root = obj.insert(obj.root, 2);
         obj.root = obj.insert(obj.root, 6);
         obj.root = obj.insert(obj.root, 1);
         obj.root = obj.insert(obj.root, 3);
-        
-        
         obj.inorder(obj.root);
+        boolean found = obj.search(obj.root, 2);
+        System.out.println(found);
     }
 }
